524a9b0022df4c7192266f0c758e5898bca652b8
XinBaoCode <1392368311@qq.com>
2023-11-25 17:12:22 +0800

    feat: init

---
+++ docs/bytedance.md
@@ -0,0 +1,5 @@
+# 字节前端面经
+
+1. [浏览器事件循环原理](https://juejin.cn/post/6844903983287549965?from=search-suggest)
+2. [通俗易懂地理解React Fiber](https://juejin.cn/post/7114132411499151373)
+3. [React Hooks完全上手指南](https://juejin.cn/post/6844904086819569672)
---
+++ js/EventLoop.js
@@ -0,0 +1,19 @@
+console.log('script start')
+setTimeout(function () {
+  console.log('setTimeout')
+}, 0);
+Promise.resolve().then(function () {
+  console.log('promise1')
+}).then(function () {
+  console.log('promise2')
+})
+console.log('script end')
+
+/**
+ * The result is as follows:
+ * script start
+ * script end
+ * promise1
+ * promise2
+ * setTimeout
+ */
